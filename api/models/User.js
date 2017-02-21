10acec60293724ddc03dc73c06b66b6469335a1d
benjiecodes <benjiecodes@gmail.com>
2017-02-21 14:49:35 -0500

    able to hash password and create login template

--- api/models/User.js
+++ api/models/User.js
@@ -1,11 +1,45 @@
+// package used for password
+var bcrypt = require('bcrypt')
+var SALT_WORK_FACTOR = 10;
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  username: String,
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
   blogs: [{type: Schema.Types.ObjectId, ref: 'Blog'}]
 });
+
+// Method used to check password before being saved to database
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+  // generate a "salt"
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Method used to verify password
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
